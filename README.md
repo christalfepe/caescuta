b02e3c0b0afc9f1810ed6b2a146514d4620453c5
Christiane Peressutti <78488694+christalfepe@users.noreply.github.com>
2023-06-19 20:12:41 -0300

    Upload do projeto

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # caescuta
 Trabalho feito para disciplina de Desenvolvimento de Sistemas WEB.
 Plataforma de gestão contínua de pessoas voltado para alunos da universidade, onde estes podem dar feedbacks quanto a espaços e/ou disciplinas.
+
+## Instalações
+É recomendado o uso da IDE PyCharm.
+
+Instalações necessárias:
+
+Python
+
+Django:
+
+        pip install django==3.1.4
+
+Opcional:
+
+        -m pip --upgrade pip
+
+## Execução do projeto
+Para execução do projeto é necessário o comando
+
+        python manage.py runserver
--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .forms import ModelUsuarioCreateForm, ModelUsuarioChangeForm
+from .models import ModelUsuario
+
+@admin.register(ModelUsuario)
+class ModelUsuarioAdmin(UserAdmin):
+    add_form = ModelUsuarioCreateForm
+    form = ModelUsuarioChangeForm
+    model = ModelUsuario
+    list_display = ('first_name', 'last_name', 'email', 'fone', 'is_staff')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Informações pessoais', {'fields': ('first_name', 'last_name', 'fone')}),
+        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Datas importantes', {'fields': ('last_login','date_joined')}),
+    )
