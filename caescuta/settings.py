cc7c7d96df3b2f3ed2a3e671ce896dac519a3094
Christiane Peressutti <78488694+christalfepe@users.noreply.github.com>
2023-07-03 18:28:18 -0300

    atualização do projeto

--- caescuta/settings.py
+++ caescuta/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'CAescuta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,10 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/static')
+]
 
 AUTH_USER_MODEL = 'usuarios.ModelUsuario'
 
