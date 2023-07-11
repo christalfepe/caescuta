d1418e063ad7bdebc9d4f02045c0fc077926ab23
Christiane Peressutti <78488694+christalfepe@users.noreply.github.com>
2023-07-11 10:54:28 -0300

    atualização do projeto

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
-from .views import Usuario
+from .views import Usuario, Admin
 
 urlpatterns = [
     path('contas/', include('django.contrib.auth.urls')),# incluindo as rotas de autenticação do próprio Django
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('usuarios/lista',Usuario.as_view(),name='usuario'),
+    path('admin/',Admin.as_view(),name='admin'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView
 from .models import *
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 from .models import *
 
@@ -9,3 +9,7 @@ from .models import *
 class Usuario(ListView):
     model = ModelUsuario
     template_name = "usuario.html"
+
+class Admin(CreateView):
+    model = UsuarioManager
+    template_name = "admin.html"
